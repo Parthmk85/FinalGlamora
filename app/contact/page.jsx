2bd67f69de4d083e4e1e7b82497ae335d8205fac
parthmk85 <parthmk85@gmail.com>
2026-01-09 12:52:48 +0530

    second commit

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <>
       <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-white to-blue-300 text-black px-2 sm:px-4 md:px-6 lg:px-8 xl:px-10 2xl:px-12 py-4 sm:py-6 md:py-8">
-        <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold mb-4 text-center">contact demo
+        <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold mb-4 text-center">contact 
 
         </h1>
         <p className="text-sm sm:text-base md:text-lg lg:text-xl text-center max-w-xs sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl 2xl:max-w-3xl leading-relaxed">
